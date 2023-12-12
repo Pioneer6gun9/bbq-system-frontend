81a7c25b07f629b5670ce92f5d59588cfc1a65eb
丁铭阳 <kkfrank@dingmingyangdeMacBook-Air.local>
2023-12-12 09:52:11 +0800

    Main页面更新

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,23 +6,52 @@
                 <img src="../assets/logo.svg" alt="logo" />
                 </router-link>
             </div>
-            <div class="header-right">
-                <div class="header-right-item">
-                    <div class="user-avatar">
-                        <el-avatar
-                            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
-                        />
-                    </div>
+            <el-popover
+            :width="300"
+            popper-style="box-shadow: rgb(14 18 22 / 35%) 0px 10px 38px -10px, rgb(14 18 22 / 20%) 0px 10px 20px -15px; padding: 20px;"
+            >
+            <template #reference>
+                <el-avatar src="https://avatars.githubusercontent.com/u/72015883?v=4" />
+            </template>
+            <template #default>
+                <div
+                class="rich-content"
+                style="display: flex; gap: 8px; flex-direction: column"
+                >
+                <span class="user-name" @click="goToUserProfile">{{ userName }}</span>
+                <span @click="goToUserProfile">个人中心</span>
+                <span @click="showLogoutConfirm">退出</span>
                 </div>
-            </div>
+            </template>
+            </el-popover>
         </div>
     </div>
 </template>
 
 <script setup>
 import { ElAvatar } from "element-plus";
+import { ref } from "vue";
+import { ElMessageBox } from "element-plus";
+import { ElPopover } from "element-plus";
 
+const visible = ref(false);
+const userName = ref('用户名'); 
 
+const goToUserProfile = () => {
+  router.push({ path: '/user' });
+};
+
+const showLogoutConfirm = () => {
+  ElMessageBox.confirm('确定退出登录吗?', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning',
+  }).then(() => {
+    console.log('用户点击了确定');
+  }).catch(() => {
+    console.log('用户点击了取消');
+  });
+};
 
 </script>
 
@@ -39,7 +68,7 @@ header {
 
 .header-wrapper {
     width: 90vw;
-    height: 100vh;
+    height: 60px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -58,6 +87,16 @@ header {
     padding-right: 10px;
 }
 
+.user-avatar {
+  width: 28px;
+  height: 28px;
+  border-radius: 50%;
+  margin-right: 8px;
+}
+
+.user-name {
+  font-size: 14px;
+}
 
 
 </style>
